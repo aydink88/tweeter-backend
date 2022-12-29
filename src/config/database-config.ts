7ed7f109a8a9689694b6a50614fb8df5b705dbd4
aydink88 <aydink88@gmail.com>
2022-12-30 00:22:42 +0300

    mysql database added

--- src/config/database-config.ts
+++ src/config/database-config.ts
@@ -7,10 +7,25 @@ import {
 } from 'src/entities';
 import { DataSourceOptions } from 'typeorm';
 
+// const databaseConfig: DataSourceOptions = {
+//   name: 'default',
+//   type: 'better-sqlite3',
+//   database: process.env.DB_NAME,
+//   //entities: [__dirname + '/../entities/*.entity.ts'],
+//   entities: [UserEntity, TweetEntity, CommentEntity, HashtagEntity],
+//   migrations: [__dirname + '/../migrations/*{.ts,.js}'],
+//   synchronize: false,
+//   logging: true,
+//   migrationsRun: false,
+// };
+
 const databaseConfig: DataSourceOptions = {
   name: 'default',
-  type: 'better-sqlite3',
-  database: process.env.DB_NAME,
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
   //entities: [__dirname + '/../entities/*.entity.ts'],
   entities: [UserEntity, TweetEntity, CommentEntity, HashtagEntity],
   migrations: [__dirname + '/../migrations/*{.ts,.js}'],
